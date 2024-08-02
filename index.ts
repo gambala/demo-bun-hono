121f5663ec4da635cad0a1da4534a313e8071897
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2024-08-02 21:37:07 +0200

    Add JSX page

--- index.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from 'hono';
-
-const app = new Hono();
-
-app.get('/', (c) => c.text('Hello, Hono with Bun!'));
-
-app.get('/hello', (c) => c.json({ message: 'Hello, World!' }));
-
-export default app;
